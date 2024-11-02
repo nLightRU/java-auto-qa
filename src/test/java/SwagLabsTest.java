513958159143cdafb8e762e15efd167585b0fd0a
Ilya <northenlight6899@gmail.com>
2024-11-02 21:39:54 +0300

    login test for fake shop added

---
+++ src/test/java/SwagLabsTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SwagLabsTest {
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://www.saucedemo.com";
+    }
+
+    @Test
+    @Order(1)
+    void TestLogin() {
+        open("/");
+        $("#user-name").setValue("standard_user");
+        $("#password").setValue("secret_sauce");
+        $("#login-button").click();
+        $(by("data-test", "title")).shouldHave(text("Products"));
+    }
+}
